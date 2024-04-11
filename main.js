971fcc66e9fc03fc1e6d0a61fddfcd4dc723c456
Hoomam <hoaz1600@student.miun.se>
2024-04-11 20:36:25 +0200

    td editable

--- main.js
+++ main.js
@@ -10,23 +10,32 @@ async function fetchData() {
         for(let entry of experience){
             let id = entry.id;
             const tr = document.createElement("tr");
-            tr.contentEditable = true;
+            /* tr.contentEditable = true; */
             tr.classList.add("exp");
             const td1 = document.createElement("td");
             td1.classList.add(id);
             td1.textContent = entry.companyname;
+            td1.contentEditable = true;
             const td2 = document.createElement("td");
             td2.classList.add(id);
             td2.textContent = entry.jobtitle;
+            td2.contentEditable = true;
             const td3 = document.createElement("td");
             td3.classList.add(id);
             td3.textContent = entry.location;
+            td3.contentEditable = true;
             const td4 = document.createElement("td");
             td4.classList.add(id);
+            td4.contentEditable = true;
             td4.textContent = entry.startdate;
             const td5 = document.createElement("td");
             td5.classList.add(id);
-            td5.textContent = entry.enddate;
+            if(entry.enddate == "1899-11-29T23:00:00.000Z"){
+                td5.textContent = "Pågående";
+            }else{
+                td5.textContent = entry.enddate;
+            }
+            td5.contentEditable = true;
             const deleteBtn = document.createElement("button");
             deleteBtn.textContent = "Delete";
             deleteBtn.addEventListener("click", ()=>{
@@ -49,11 +58,7 @@ async function fetchData() {
         
     } catch (error) {
         console.error('Fetch error:', error);
-        /* errorDiv.style.display = 'block';
-        errorDiv.innerHTML = ` Data är ej tillgänglig: ${error}` */
-    } finally{
-       
-    }  
+    }
 } 
 
 
@@ -136,7 +141,7 @@ addBtn.addEventListener("click", (event)=>{
     let startdate = startDate.value;
     let enddate = endDate.value;
     
-    if(companyname && jobtitle && location && startdate && enddate){
+    if(companyname && jobtitle && location && startdate){
         postData(companyname, jobtitle, location, startdate, enddate);
     }else{
 
@@ -174,3 +179,4 @@ async function postData(companyname, jobtitle, location, startdate, enddate){
         }
     }
 }
+
