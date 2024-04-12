5d19e20a87a41553c74be877eee462fa4ec2fc95
Hoomam <hoaz1600@student.miun.se>
2024-04-12 22:20:08 +0200

    Deployment troubleshooting

--- server.js
+++ server.js
@@ -14,8 +14,35 @@ const pool = mysql.createPool({
     user: process.env.CV_USER,
     password: process.env.CV_PASSWORD,
     database: process.env.CV_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
   });
 
+//Deploment troubleshooting
+pool.query(`
+  CREATE TABLE IF NOT EXISTS workexperience (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    companyname VARCHAR(30) NOT NULL,
+    jobtitle VARCHAR(30) NOT NULL,
+    location VARCHAR(30) NOT NULL,
+    startdate DATE NOT NULL,
+    enddate DATE
+  );`,
+     (err, results)=> {
+      if(err){
+        console.error(err);
+        console.log("Kolla: " + err);
+      }else{
+        console.log(results);
+      }
+    }
+  );
+  // ******
  
 
 // Hantera GET requests
